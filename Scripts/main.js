aa13aa9db26b7462cd160ec554e71d99e9ed412d
Matt Derosier <derosier.matt@gmail.com>
2018-12-31 14:28:36 -0600

    Trimmed the fat, kinda.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,4 @@
 //Private Key located in hidden JS file
-var mycid = config.MY_CID
-var myapikey = config.API_KEY
+var mycid = config.MY_CID;
+var myapikey = config.API_KEY;
+var secretsauce = config.SHEET_ID;
